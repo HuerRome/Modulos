c691b4476d576b31b45157c2d800b8ef9a04de8d
Romero Ana <97923571+HuerRome@users.noreply.github.com>
2023-06-20 07:46:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var wrapperMenu = document.querySelector('.menu-outline');
+var Menu = document.querySelector('.container .menu');
+
+wrapperMenu.addEventListener('click', function(){
+  Menu.classList.toggle('active');  
+})
+
